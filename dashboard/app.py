6fff7fc4a3f738456587bf63ec112a9f4996994b
Falrlz <m.falahakbar.a@gmail.com>
2026-05-01 14:47:53 +0700

    fix import order for deployment

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+# inject root project ke PYTHONPATH (HARUS paling atas)
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+
 import streamlit as st
 import pandas as pd
 
